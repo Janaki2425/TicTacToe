565612cac649410e1466c6a8911ec56055944527
Janaki2425 <96913266+Janaki2425@users.noreply.github.com>
2022-02-14 19:02:11 +0530

    create board

--- CreateBoard.cs
+++ CreateBoard.cs
@@ -8,5 +8,13 @@ namespace TicTacToeGame
 {
     internal class CreateBoard
     {
+        char[] position = new char[10] { ' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',' ' };  
+
+        public  void MakeBoard() 
+        {
+            Console.WriteLine("{0}    {1}    {2}", position[1], position[2], position[3]);
+            Console.WriteLine("{0}    {1}    {2}", position[4], position[5], position[6]);
+            Console.WriteLine("{0}    {1}    {2}", position[7], position[8], position[9]);
+        }
     }
 }
